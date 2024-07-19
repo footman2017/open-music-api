12a66d4ab1ba7691755525f42178e9f9285dedba
Raey Faldo <raey3221@gmail.com>
2024-07-19 11:49:58 +0700

    init

---
+++ src/api/songs/routes.js
@@ -0,0 +1,29 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: (request, h) => handler.postAlbumHandler(request, h),
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: () => handler.getAlbumsHandler(),
+  },
+  {
+    method: 'GET',
+    path: '/songs/{id}',
+    handler: (request, h) => handler.getAlbumByIdHandler(request, h),
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{id}',
+    handler: (request, h) => handler.putAlbumByIdHandler(request, h),
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{id}',
+    handler: (request, h) => handler.deleteAlbumByIdHandler(request, h),
+  },
+];
+
+module.exports = routes;
